ff1624966619c3bfc2455acd1da7f6f3d0bc6b5f
John Grudzinski <johngrudzinski@yahoo.com>
2023-01-31 20:43:41 -0500

    Mobile Commit 6

--- script.js
+++ script.js
@@ -3,14 +3,23 @@ const overlayAnim = document.querySelector('#overlay');
 const overlayAnim2 = document.querySelector('#overlay2');
 const fadeElems = document.querySelectorAll('.has-fade');
 const fadeElems2 = document.querySelectorAll('.has-fade2');
-const features = document.querySelector('.features');
-const submenu = document.querySelector('.features-submenu');
+const featuresSelector = document.querySelector('.features-subM');
+const featuresSubmenu = document.querySelector('.features-submenu');
 const arrow1 = document.querySelector('.down-arrow1');
+const companySelector = document.querySelector('.company-subM');
+const companySubmenu = document.querySelector('.company-submenu');
+const arrow2 = document.querySelector('.down-arrow2');
 
 btnHamburger.addEventListener('click', function(){
 
     if(btnHamburger.classList.contains('header-open')){ /* close Hamburger menu */
         btnHamburger.classList.remove('header-open');
+        featuresSubmenu.classList.toggle('hidden');
+        featuresSubmenu.classList.remove('show');
+        arrow1.classList.remove("active");
+        companySubmenu.classList.toggle('hidden');
+        companySubmenu.classList.remove('show');
+        arrow2.classList.remove("active");
         fadeElems.forEach(function(element){
             element.classList.remove('fade-in');
             element.classList.add('fade-out');
@@ -46,7 +55,34 @@ btnHamburger.addEventListener("click", function() {
 
 // Reveals the submenu for Features
 
-features.addEventListener('click', function() {
-    arrow1.classList.toggle('active');
-    submenu.classList.toggle('show');
+featuresSelector.addEventListener('click', function() {
+    if (!featuresSubmenu.classList.contains('show')) {
+        featuresSubmenu.classList.remove('hidden');
+        arrow1.classList.toggle('active');
+        featuresSubmenu.classList.toggle('show');
+    }
+    else {
+        featuresSubmenu.classList.toggle('hidden');
+        featuresSubmenu.classList.remove('show');
+        arrow1.classList.toggle("active");
+    }
 });
+
+
+
+companySelector.addEventListener('click', function() {
+    if (!companySubmenu.classList.contains('show')) {
+        companySubmenu.classList.remove('hidden');
+        arrow2.classList.toggle('active');
+        companySubmenu.classList.toggle('show');
+    }
+    else {
+        companySubmenu.classList.toggle('hidden');
+        companySubmenu.classList.remove('show');
+        arrow2.classList.toggle("active");
+    }
+})
+
+
+
+/* submenu.style.display = submenu.classList.contains("show") ? "inline-block" : "none"; */
